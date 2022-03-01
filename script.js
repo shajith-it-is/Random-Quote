4235828814e5f86d73b2ab412be43d7c858ab8c8
ShajithKAJ <jshajithkhanafridi@gmail.com>
2022-03-01 19:28:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let i=2;
+
+function next() {
+    i++;
+    loader();
+    const imageElement = document.querySelector('#image img');
+    imageElement.setAttribute("src",`./assets/2 (${i}).jpg`);
+}
+
+function previous() {
+    i--;
+    loader();
+    const imageElement = document.querySelector('#image img');
+    imageElement.setAttribute("src",`./assets/2 (${i}).jpg`);
+}
+
+function loader() {
+    const loader = document.querySelector('.loader');
+    const div = document.querySelector('#image');
+    div.style.display = 'none';
+    loader.style.display = 'block';
+    document.querySelector('body').classList.add('modal-screen');
+    
+    setTimeout(() => {
+        loader.style.display = 'none';
+        div.style.display = 'block';
+        document.querySelector('body').classList.remove('modal-screen');
+    }, 750);
+}
